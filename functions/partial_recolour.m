dd7bef74b6159540478844fc897131f840b72d26
Ben Walker <38265558+Benjamin-Walker@users.noreply.github.com>
2021-01-28 14:04:37 +0000

    Update partial_recolour.m

--- functions/partial_recolour.m
+++ functions/partial_recolour.m
@@ -1,5 +1,7 @@
 function P = partial_recolour(G,C,D)
 
+    % Given a matrix of 1s where you want colour and 0s where
+    % you don't produce a partially coloured image
     inv_D = uint8(ones(size(D))-D);
     D = uint8(D);
     P = inv_D.*G+D.*C;
