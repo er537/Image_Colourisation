1304cc15d366d3bfb794b89d985d92f0de05267d
Ben Walker <38265558+Benjamin-Walker@users.noreply.github.com>
2021-01-28 14:02:40 +0000

    Update random_grid.m

--- functions/random_grid.m
+++ functions/random_grid.m
@@ -1,10 +1,9 @@
 function D = random_grid(G,proportion)
 
     G_size = size(G);
-    height = round(rand(1,round(sqrt(proportion)*G_size(1)))*G_size(1));
-    width = round(rand(1,round(sqrt(proportion)*G_size(2)))*G_size(2));
-    H = full(sparse(1,height,1,1,G_size(1)));
-    W = full(sparse(1,width,1,1,G_size(2)));
-    D = H'*W;
+    % take a logical array of whether a random number between 0 and 1 is 
+    % less than proportion and reshape it to the size of the image
+    D = reshape(rand(1,G_size(1)*G_size(2))<proportion,G_size(1),G_size(2));
+
 
 end
